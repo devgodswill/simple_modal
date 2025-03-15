00a9a9e27c3a28ecc9da8c8e36d3787eaefec8c3
devgodswill <godswillnathaniel97@gmail.com>
2025-03-15 06:38:54 +0100

    final commit

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnClose = document.querySelector('.close-modal');
+const btnShow = document.querySelectorAll('.show-modal');
+for (let i = 0; i < btnShow.length; i++)
+    btnShow[i].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+
+btnClose.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
